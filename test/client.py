9e6d35778bf2e7b360ce3e7110fe54f5873ce8d4
musicn <nm4063@student.uni-lj.si>
2024-12-03 14:10:13 +0100

    added docker

--- test/client.py
+++ test/client.py
@@ -14,26 +14,9 @@ def run():
     
     # Send a test email
     response = stub.SendEmail(email_pb2.EmailRequest(
-        recipient_email="matej.kovack@gmail.com",
-        subject="Obvestilo o naročnini na storitev odrasle vsebine",
-        message="""Spoštovani Matej Mateo,
-                    prejšnji teden ste uspešno opravili naročilo na storitev Paket ultra XXX, ki vam omogoča dostop do ekskluzivne odrasle vsebine. V skladu s pogoji naročnine bo vaša kartica mesečno bremenjena za znesek 9,99 EUR.
-
-                    Podrobnosti naročnine:
-
-                    Ime naročnine: Paket ultra XXX
-                    Znesek: 9,99 EUR
-                    Interval: mesečno
-                    Datum prvega bremena: 5.12.2024
-                    Če želite upravljati svojo naročnino, jo preklicati ali potrebujete dodatne informacije, nas lahko kontaktirate preko:
-
-                    E-pošte: tvoj.prijatelj@gmail.com
-                    Telefonske številke: 041 456 789
-                    Zahvaljujemo se vam za zaupanje in želimo prijetno uporabo naših storitev.
-
-                    Lep pozdrav,
-                    Vodja ekipe Vedno Zadoscen, dipl. ing. Gal Menase
-                    """
+        recipient_email="gal.menase@gmail.com",
+        subject="Super Vsebina",
+        message="""Zelo informativno sporocilo"""
     ))
     print("Response from server:", response.status)
 
